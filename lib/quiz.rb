c0316142388651854cca36ab17cad5daa1877d9a
Soroush Khosravi <sorush.kh68@gmail.com>
2019-04-10 15:45:27 +0100

    We are able to check the answer.

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -2,4 +2,8 @@ class Quiz
   def show_question
     "What is the sum of 1 and 2"
   end
+
+  def check_answer(answer)
+    return "Your answer is correct" if answer =="3"
+  end
 end
--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -4,4 +4,9 @@ describe Quiz do
     quiz = Quiz.new()
     expect(quiz.show_question).to eq "What is the sum of 1 and 2"
   end
+
+  it "We can cheack the answer" do
+    quiz = Quiz.new()
+    expect(quiz.check_answer("3")).to eq "Your answer is correct"
+  end
 end
